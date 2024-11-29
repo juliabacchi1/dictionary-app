b8db1844add5ad48f00b3045cc7a748596e35c00
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-11-29 20:23:54 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
