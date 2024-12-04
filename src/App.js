a7e99c1afeead842049f5419bab2b98160aa96af
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-12-04 12:32:31 -0300

    Added dictionary api integration, included the footers information

--- src/App.js
+++ src/App.js
@@ -14,7 +14,32 @@ function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by</small></footer>
+          This project was coded by{" "}
+          <a
+            href="https://portfolio-juliabacchi.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Júlia Bacchi
+          </a>
+          . Is open-sourced on{" "}
+          <a
+            href="https://github.com/juliabacchi1/dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://dictionary-app-search.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0].definition);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    // alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "4b59c6fof3bta7634229de85af0382e7";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
@@ -17,7 +27,13 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          placeholder="Searching for..."
+          autofocus
+          required
+        />
       </form>
     </div>
   );
