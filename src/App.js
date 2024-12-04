1a263f07b6b19b97bfb874742f15586bb5aa496b
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2024-12-04 11:17:06 -0300

    Added the search engine at Dictionary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import React from "react";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by</small></footer>
+      </div>
     </div>
   );
 }
